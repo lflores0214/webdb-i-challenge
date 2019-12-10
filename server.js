50e9a2eaee61788692afefa48a24fb7258214f5b
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-12-09 17:38:59 -0700

    Finished mvp

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const db = require('./data/dbConfig.js');
 
+const AccountsRouter = require('./Accounts/accounts-router');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', AccountsRouter)
+
+server.use('/', (req,res)=> {
+    res.send('Hello')
+})
 
 module.exports = server;
