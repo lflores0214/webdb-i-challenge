768ab94f03c72f89e1056337e05a222e257bcb97
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-12-09 18:57:50 -0700

    finsished queries.sql stretch

--- queries.sql
+++ queries.sql
@@ -26,5 +26,9 @@ values
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
 update customers set PostalCode = 111222 where CustomerId = 92
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
-
+SELECT count(distinct city)
+FROM Customers
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+SELECT SupplierName
+FROM [Suppliers]
+where length(SupplierName) Between 20 and 50
